4d632ad58c846c75e7e01ad14d8b006f3308db88
nohsueh <nohsueh@qq.com>
2025-05-10 07:26:59 +0800

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,27 +4,33 @@ import {
 } from "@opennextjs/cloudflare";
 import type { NextConfig } from "next";
 
-initOpenNextCloudflareForDev();
+export default async function ({
+  defaultConfig,
+}: {
+  defaultConfig: NextConfig;
+}): Promise<NextConfig> {
+  initOpenNextCloudflareForDev(); 
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  basePath: getCloudflareContext().env.NEXT_PUBLIC_BASE_PATH || "",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
-  experimental: {
-    serverActions: {
-      allowedOrigins: [
-        getCloudflareContext().env.NEXT_PUBLIC_ROOT_DOMAIN || "searchlysis.com",
-        `*.${getCloudflareContext().env.NEXT_PUBLIC_ROOT_DOMAIN || ".searchlysis.com"}`,
+  const ROOT = getCloudflareContext().env.NEXT_PUBLIC_ROOT_DOMAIN || "searchlysis.com";
+  const allowedOrigins = [ROOT, `*.${ROOT}`];
+
+  const nextConfig: NextConfig = {
+    ...defaultConfig,
+    basePath: getCloudflareContext().env.NEXT_PUBLIC_BASE_PATH || "",
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "**",
+        },
       ],
     },
-  },
-};
+    experimental: {
+      serverActions: {
+        allowedOrigins,
+      },
+    },
+  };
 
-export default nextConfig;
+  return nextConfig;
+}
