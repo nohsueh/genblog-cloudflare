e18cbe647b0453de2f188c36c47210f0816e4de4
nohsueh <nohsueh@qq.com>
2025-05-25 12:16:06 +0800

    update 22 files

--- app/[language]/layout.tsx
+++ app/[language]/layout.tsx
@@ -3,7 +3,6 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { i18n } from "@/lib/i18n-config";
 import { GoogleAnalytics } from "@next/third-parties/google";
-import { LoaderCircle } from "lucide-react";
 import { Inter } from "next/font/google";
 import Script from "next/script";
 import type React from "react";
@@ -25,33 +24,27 @@ export default async function RootLayout(props: RootLayoutProps) {
   return (
     <html lang={language} suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Suspense
-            fallback={
-              <div className="flex h-[50vh] w-screen flex-col items-center justify-center">
-                <LoaderCircle className="size-10 animate-[spin_0.4s_linear_infinite]" />
-              </div>
-            }
+        <Suspense>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
           >
             {children}
-          </Suspense>
-          <Toaster richColors />
-        </ThemeProvider>
-        {process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_ACCOUNT && (
-          <Script id="google-adsense-init">
-            {`(adsbygoogle = window.adsbygoogle || []).push({});`}
-          </Script>
-        )}
-        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID && (
-          <GoogleAnalytics
-            gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID}
-          />
-        )}
+            <Toaster richColors />
+          </ThemeProvider>
+          {process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_ACCOUNT && (
+            <Script id="google-adsense-init">
+              {`(adsbygoogle = window.adsbygoogle || []).push({});`}
+            </Script>
+          )}
+          {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID && (
+            <GoogleAnalytics
+              gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID}
+            />
+          )}
+        </Suspense>
       </body>
     </html>
   );
